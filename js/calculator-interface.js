53eb0cbac0596fab85c1cd6b5105c274a55ce1f7
alvin niamatali and daniel cheng <alvin@>
2017-11-14 08:51:26 -0800

    let and const truly working moved it back to the the interface js because it wont work in the back

--- js/calculator-interface.js
+++ js/calculator-interface.js
@@ -3,12 +3,31 @@ var Calculate = require('../js/calculator.js').calculatorModule;
 $(document).ready(function() {
   $("#calcInput").submit(function(event) {
     event.preventDefault();
-        const operator = $("input:radio[name=operator]:checked").val();
-        const number1 = parseInt($("#valInput").val());
-        const number2 = parseInt($("#valInput2").val());
-        const thisCalculate = new Calculate();
-      
+    const operator = $("input:radio[name=operator]:checked").val();
+    const number1 = parseInt($("#valInput").val());
+    const number2 = parseInt($("#valInput2").val());
+    const thisCalculate = new Calculate();
+    const output = "the answer";
 
+    if (operator === "add") {
+      let output = thisCalculate.Add(number1, number2);
+      $("#output").prepend("<li>"+output+"</li>");
+    }  else if (operator === "subtract") {
+      let output = thisCalculate.Subtract(number1, number2);
+      $("#output2").prepend("<li>"+output+"</li>");
+    } else if (operator === "multiply") {
+      let output = thisCalculate.Multiply(number1, number2);
+      $("#output3").prepend("<li>"+output+"</li>");
+    } else if (operator === "divide") {
+      let output = thisCalculate.Divide(number1, number2);
+      $("#output4").prepend("<li>"+output+"</li>");
+    } else if (operator === "exponent") {
+      let output = thisCalculate.Exponent(number1, number2);
+      $("#output5").prepend("<li>"+output+"</li>");
+    }  else if (operator === "percent") {
+      let output = thisCalculate.Percent(number1, number2);
+      $("#output6").prepend("<li>"+output+"</li>");
+    }
 
       });
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -3,31 +3,31 @@ var Calculate = require('../js/calculator.js').calculatorModule;
 $(document).ready(function() {
   $("#calcInput").submit(function(event) {
     event.preventDefault();
-        const operator = $("input:radio[name=operator]:checked").val();
-        const number1 = parseInt($("#valInput").val());
-        const number2 = parseInt($("#valInput2").val());
-        const thisCalculate = new Calculate();
-        const output1 = thisCalculate.Add(number1, number2);
-        const output2 = thisCalculate.Subtract(number1, number2);
-        const output3 = thisCalculate.Multiply(number1, number2);
-        const output4 = thisCalculate.Divide(number1, number2);
-        const output5 = thisCalculate.Exponent(number1, number2);
-        const output6 = thisCalculate.Percent(number1, number2);
+    const operator = $("input:radio[name=operator]:checked").val();
+    const number1 = parseInt($("#valInput").val());
+    const number2 = parseInt($("#valInput2").val());
+    const thisCalculate = new Calculate();
+    const output = "the answer";
 
-        if (operator === "add") {
-          $("#output").prepend("<li>"+output1+"</li>");
-        } else if (operator === "subtract") {
-          $("#output2").prepend("<li>"+output2+"</li>");
-        } else if (operator === "multiply") {
-          $("#output3").prepend("<li>"+output3+"</li>");
-        } else if (operator === "divide") {
-          $("#output4").prepend("<li>"+output4+"</li>");
-        } else if (operator === "exponent") {
-          $("#output5").prepend("<li>"+output5+"</li>");
-        } else if (operator === "percent") {
-          $("#output6").prepend("<li>"+output6+"</li>");
-        }
-
+    if (operator === "add") {
+      let output = thisCalculate.Add(number1, number2);
+      $("#output").prepend("<li>"+output+"</li>");
+    }  else if (operator === "subtract") {
+      let output = thisCalculate.Subtract(number1, number2);
+      $("#output2").prepend("<li>"+output+"</li>");
+    } else if (operator === "multiply") {
+      let output = thisCalculate.Multiply(number1, number2);
+      $("#output3").prepend("<li>"+output+"</li>");
+    } else if (operator === "divide") {
+      let output = thisCalculate.Divide(number1, number2);
+      $("#output4").prepend("<li>"+output+"</li>");
+    } else if (operator === "exponent") {
+      let output = thisCalculate.Exponent(number1, number2);
+      $("#output5").prepend("<li>"+output+"</li>");
+    }  else if (operator === "percent") {
+      let output = thisCalculate.Percent(number1, number2);
+      $("#output6").prepend("<li>"+output+"</li>");
+    }
 
       });
 
